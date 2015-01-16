6b67cf46ada295bc36d4017917ba3875ce46396a
h46incon <h46incon@gmail.com>
2015-01-16 21:51:50 +0800

    Get view into variable.

--- app/src/main/java/com/tools/h46incon/doorcontroler/MainActivity.java
+++ app/src/main/java/com/tools/h46incon/doorcontroler/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -15,21 +16,62 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		TextView consoleTV = (TextView) findViewById(R.id.console_tv);
-		outputConsole = new OutputConsole(consoleTV);
+		initMember();
 
-		Button BTSettingBtn = (Button) findViewById(R.id.btsetting_btn);
-		BTSettingBtn.setOnClickListener(new View.OnClickListener() {
+		btSettingBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v)
 			{
 //				startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
 				outputConsole.printNewItem("time " + System.currentTimeMillis());
+				arrowHLManager.highlight(btSettingArrowIV);
 			}
 		});
 
+		devShakehandBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				arrowHLManager.highlight(devShakeHandArrowIV);
+			}
+		});
 	}
 
-	OutputConsole outputConsole;
+	private void initMember()
+	{
+		TextView consoleTV = (TextView) findViewById(R.id.console_tv);
+		outputConsole = new OutputConsole(consoleTV);
+
+		arrowHLManager = new ArrowHLManager(R.drawable.arrow_default, R.drawable.arrow_highlight);
+
+		// get views
+		btSettingBtn = (Button) findViewById(R.id.btsetting_btn);
+		devShakehandBtn = (Button) findViewById(R.id.devShakeHand_btn);
+		openDoorBtn = (Button) findViewById(R.id.openDoor_btn);
+		exitBtn = (Button) findViewById(R.id.exit_btn);
+
+		btSettingArrowIV = (ImageView) findViewById(R.id.btSetting_arrow_iv);
+		devShakeHandArrowIV = (ImageView) findViewById(R.id.devShakeHand_arrow_iv);
+		openDoorArrowIV = (ImageView) findViewById(R.id.openDoor_arrow_iv);
+		exitArrowArrowIV = (ImageView) findViewById(R.id.exit_arrow_iv);
+
+		btSettingInfoTV = (TextView) findViewById(R.id.btInfo_tv);
+		devShakeHandInfoTV = (TextView) findViewById(R.id.devShakeHandInfo_tv);
+	}
+
+	private OutputConsole outputConsole;
+	private ArrowHLManager arrowHLManager;
+	// Views
+	private Button btSettingBtn;
+	private Button devShakehandBtn;
+	private Button openDoorBtn;
+	private Button exitBtn;
+	private ImageView btSettingArrowIV;
+	private ImageView devShakeHandArrowIV;
+	private ImageView openDoorArrowIV;
+	private ImageView exitArrowArrowIV;
+	private TextView btSettingInfoTV;
+	private TextView devShakeHandInfoTV;
+
 
 }
