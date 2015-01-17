8c2f7eceaf50c3c5ce2606d07a9af64474c49e09
h46incon <h46incon@gmail.com>
2015-01-17 09:27:36 +0800

    Add a StateManager
    This class will manger current state, highlight arrow for current state, and judge which button's click event should be handler (TODO)

--- app/src/main/java/com/tools/h46incon/doorcontroler/MainActivity.java
+++ app/src/main/java/com/tools/h46incon/doorcontroler/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tools.h46incon.doorcontroler;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +12,112 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+	private static enum  State{
+		BT_SETTING,
+		DEV_SHAKEHAND,
+		OPEN_DOOR,
+		EXIT,
+
+		_STATE_NUMBER
+
+	}
+
+	private static interface OnBTClick{
+		public void onBTClick(State state);
+	}
+
+	private class StateManager{
+		public StateManager(OnBTClick onBTClick, State initState)
+		{
+			this.onBTClick = onBTClick;
+			this.curState = initState;
+
+			initVar();
+
+			// Set button's callback
+			for (int i = 0; i < stateNum; ++i) {
+				final State currentState = State.values()[i];
+				buttons[i].setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v)
+					{
+						BTClickHandler(currentState);
+					}
+				});
+			}
+
+			arrowHLManager.highlight(imageViews[curState.ordinal()]);
+		}
+
+		public void changeState(State state)
+		{
+			curState = state;
+			arrowHLManager.highlight(imageViews[curState.ordinal()]);
+		}
+
+
+		private void BTClickHandler(State state)
+		{
+			// Todo: check current click is legal
+			onBTClick.onBTClick(state);
+		}
+
+		private void initVar()
+		{
+			// get views
+			btSettingBtn = (Button) findViewById(R.id.btsetting_btn);
+			devShakehandBtn = (Button) findViewById(R.id.devShakeHand_btn);
+			openDoorBtn = (Button) findViewById(R.id.openDoor_btn);
+			exitBtn = (Button) findViewById(R.id.exit_btn);
+
+			btSettingArrowIV = (ImageView) findViewById(R.id.btSetting_arrow_iv);
+			devShakeHandArrowIV = (ImageView) findViewById(R.id.devShakeHand_arrow_iv);
+			openDoorArrowIV = (ImageView) findViewById(R.id.openDoor_arrow_iv);
+			exitArrowIV = (ImageView) findViewById(R.id.exit_arrow_iv);
+
+			btSettingInfoTV = (TextView) findViewById(R.id.btInfo_tv);
+			devShakeHandInfoTV = (TextView) findViewById(R.id.devShakeHandInfo_tv);
+
+			// store views into array
+			buttons = new Button[stateNum];
+			buttons[State.BT_SETTING.ordinal()] = btSettingBtn;
+			buttons[State.DEV_SHAKEHAND.ordinal()] = devShakehandBtn;
+			buttons[State.OPEN_DOOR.ordinal()] = openDoorBtn;
+			buttons[State.EXIT.ordinal()] = exitBtn;
+
+			imageViews = new ImageView[stateNum];
+			imageViews[State.BT_SETTING.ordinal()] = btSettingArrowIV;
+			imageViews[State.DEV_SHAKEHAND.ordinal()] = devShakeHandArrowIV;
+			imageViews[State.OPEN_DOOR.ordinal()] = openDoorArrowIV;
+			imageViews[State.EXIT.ordinal()] = exitArrowIV;
+
+			// init other member
+			this.arrowHLManager =
+					new ArrowHLManager(R.drawable.arrow_default, R.drawable.arrow_highlight);
+		}
+
+		private final int stateNum = State._STATE_NUMBER.ordinal();
+
+		private State curState;
+		private ArrowHLManager arrowHLManager;
+		private OnBTClick onBTClick;
+
+		// Arrays of views
+		private Button[] buttons;
+		private ImageView[] imageViews;
+		// Views
+		private Button btSettingBtn;
+		private Button devShakehandBtn;
+		private Button openDoorBtn;
+		private Button exitBtn;
+		private ImageView btSettingArrowIV;
+		private ImageView devShakeHandArrowIV;
+		private ImageView openDoorArrowIV;
+		private ImageView exitArrowIV;
+		private TextView btSettingInfoTV;
+		private TextView devShakeHandInfoTV;
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -18,23 +126,7 @@ public class MainActivity extends ActionBarActivity {
 
 		initMember();
 
-		btSettingBtn.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v)
-			{
-//				startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
-				outputConsole.printNewItem("time " + System.currentTimeMillis());
-				arrowHLManager.highlight(btSettingArrowIV);
-			}
-		});
 
-		devShakehandBtn.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v)
-			{
-				arrowHLManager.highlight(devShakeHandArrowIV);
-			}
-		});
 	}
 
 	private void initMember()
@@ -42,36 +134,22 @@ public class MainActivity extends ActionBarActivity {
 		TextView consoleTV = (TextView) findViewById(R.id.console_tv);
 		outputConsole = new OutputConsole(consoleTV);
 
-		arrowHLManager = new ArrowHLManager(R.drawable.arrow_default, R.drawable.arrow_highlight);
-
-		// get views
-		btSettingBtn = (Button) findViewById(R.id.btsetting_btn);
-		devShakehandBtn = (Button) findViewById(R.id.devShakeHand_btn);
-		openDoorBtn = (Button) findViewById(R.id.openDoor_btn);
-		exitBtn = (Button) findViewById(R.id.exit_btn);
-
-		btSettingArrowIV = (ImageView) findViewById(R.id.btSetting_arrow_iv);
-		devShakeHandArrowIV = (ImageView) findViewById(R.id.devShakeHand_arrow_iv);
-		openDoorArrowIV = (ImageView) findViewById(R.id.openDoor_arrow_iv);
-		exitArrowArrowIV = (ImageView) findViewById(R.id.exit_arrow_iv);
-
-		btSettingInfoTV = (TextView) findViewById(R.id.btInfo_tv);
-		devShakeHandInfoTV = (TextView) findViewById(R.id.devShakeHandInfo_tv);
+		OnBTClick onBTClick = new OnBTClick() {
+			@Override
+			public void onBTClick(State state)
+			{
+				switch (state) {
+					case BT_SETTING:
+						startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
+						break;
+				}
+			}
+		};
+		stateManager = new StateManager(onBTClick, State.BT_SETTING);
 	}
 
 	private OutputConsole outputConsole;
-	private ArrowHLManager arrowHLManager;
-	// Views
-	private Button btSettingBtn;
-	private Button devShakehandBtn;
-	private Button openDoorBtn;
-	private Button exitBtn;
-	private ImageView btSettingArrowIV;
-	private ImageView devShakeHandArrowIV;
-	private ImageView openDoorArrowIV;
-	private ImageView exitArrowArrowIV;
-	private TextView btSettingInfoTV;
-	private TextView devShakeHandInfoTV;
+	private StateManager stateManager;
 
 
 }
